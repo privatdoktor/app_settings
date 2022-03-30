52fcff8f0d1693c1dc926c7f9ec52397f4138ad8
Oliver Kocsis <okocsis88@gmail.com>
2022-03-30 17:11:55 +0200

    refactor to properly await the completion of each native call; propagating back any exception to flutter if such occurs; activity field is now correctly saved and safely queried when intent is created

--- android/src/main/kotlin/com/example/appsettings/AppSettingsPlugin.kt
+++ android/src/main/kotlin/com/example/appsettings/AppSettingsPlugin.kt
@@ -1,132 +1,169 @@
 package com.example.appsettings
 
-import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 class AppSettingsPlugin() : MethodCallHandler, FlutterPlugin, ActivityAware {
     /// Private variable to hold instance of Registrar for creating Intents.
-    private lateinit var activity: Activity
+    private var binding: FlutterPluginBinding? = null
+    private var activityBinding: ActivityPluginBinding? = null
 
     /// Private method to open device settings window
     private fun openSettings(url: String, asAnotherTask: Boolean = false) {
+        val activity = activityBinding?.activity
+        if (activity == null) {
+            return
+        }
         try {
             val intent = Intent(url)
             if (asAnotherTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            this.activity.startActivity(intent)
-        } catch (e: Exception) {
+            activity.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
             // Default to APP Settings if setting activity fails to load/be available on device
             openAppSettings(asAnotherTask)
         }
     }
 
-    private fun openAppSettings(asAnotherTask: Boolean = false) {
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-        if (asAnotherTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        val uri = Uri.fromParts("package", this.activity.packageName, null)
-        intent.data = uri
-        this.activity.startActivity(intent)
-    }
-
-    /// Main constructor to setup the Registrar
-    constructor(registrar: Registrar) : this() {
-        this.activity = registrar.activity()
-    }
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "app_settings")
-            channel.setMethodCallHandler(AppSettingsPlugin(registrar))
+    private fun openNotification(asAnotherTask: Boolean) {
+        val activity = activityBinding?.activity
+        if (activity == null) {
+            return
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                .putExtra(Settings.EXTRA_APP_PACKAGE, activity.packageName)
+            if (asAnotherTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            activity.startActivity(intent)
+        } else {
+            openSettings(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS, asAnotherTask)
         }
     }
 
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    private fun openAppSettings(asAnotherTask: Boolean = false) {
+        val activity = activityBinding?.activity
+        if (activity == null) {
+            return
+        }
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        if (asAnotherTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val uri = Uri.fromParts("package", activity.packageName, null)
+        intent.data = uri
+        activity.startActivity(intent)
+    }
+
+
+    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+        this.binding = binding
         val channel = MethodChannel(binding.binaryMessenger, "app_settings")
         channel.setMethodCallHandler(this)
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+        this.binding = null
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        this.activity = binding.activity
+        activityBinding = binding
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
+        activityBinding = null
     }
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        this.activity = binding.activity
+        activityBinding = binding
     }
 
     override fun onDetachedFromActivity() {
+        activityBinding = null
     }
 
     /// Handler method to manage method channel calls.
-    override fun onMethodCall(call: MethodCall, result: Result) {
-
+    private fun handle(call: MethodCall) {
         val asAnotherTask = call.argument("asAnotherTask") ?: false
 
-        if (call.method == "wifi") {
-            openSettings(Settings.ACTION_WIFI_SETTINGS, asAnotherTask)
-        } else if (call.method == "wireless") {
-            openSettings(Settings.ACTION_WIRELESS_SETTINGS, asAnotherTask)
-        } else if (call.method == "location") {
-            openSettings(Settings.ACTION_LOCATION_SOURCE_SETTINGS, asAnotherTask)
-        } else if (call.method == "security") {
-            openSettings(Settings.ACTION_SECURITY_SETTINGS, asAnotherTask)
-        } else if (call.method == "bluetooth") {
-            openSettings(Settings.ACTION_BLUETOOTH_SETTINGS, asAnotherTask)
-        } else if (call.method == "data_roaming") {
-            openSettings(Settings.ACTION_DATA_ROAMING_SETTINGS, asAnotherTask)
-        } else if (call.method == "date") {
-            openSettings(Settings.ACTION_DATE_SETTINGS, asAnotherTask)
-        } else if (call.method == "display") {
-            openSettings(Settings.ACTION_DISPLAY_SETTINGS, asAnotherTask)
-        } else if (call.method == "notification") {
-            if (Build.VERSION.SDK_INT >= 21) {
-                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-                        .putExtra(Settings.EXTRA_APP_PACKAGE, this.activity.packageName)
-                if (asAnotherTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                this.activity.startActivity(intent)
-            } else {
-                openSettings(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS, asAnotherTask)
+        when (call.method) {
+            "wifi" -> {
+                openSettings(Settings.ACTION_WIFI_SETTINGS, asAnotherTask)
             }
-        } else if (call.method == "nfc") {
-            openSettings(Settings.ACTION_NFC_SETTINGS, asAnotherTask)
-        } else if (call.method == "sound") {
-            openSettings(Settings.ACTION_SOUND_SETTINGS, asAnotherTask)
-        } else if (call.method == "internal_storage") {
-            openSettings(Settings.ACTION_INTERNAL_STORAGE_SETTINGS, asAnotherTask)
-        } else if (call.method == "battery_optimization") {
-            openSettings(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS, asAnotherTask)
-        } else if (call.method == "vpn") {
-            if(Build.VERSION.SDK_INT >= 24) {
-                openSettings(Settings.ACTION_VPN_SETTINGS, asAnotherTask)
-            }else{
-                openSettings("android.net.vpn.SETTINGS", asAnotherTask)
+            "wireless" -> {
+                openSettings(Settings.ACTION_WIRELESS_SETTINGS, asAnotherTask)
             }
-        } else if (call.method == "app_settings") {
-            openAppSettings(asAnotherTask)
+            "location" -> {
+                openSettings(Settings.ACTION_LOCATION_SOURCE_SETTINGS, asAnotherTask)
+            }
+            "security" -> {
+                openSettings(Settings.ACTION_SECURITY_SETTINGS, asAnotherTask)
+            }
+            "bluetooth" -> {
+                openSettings(Settings.ACTION_BLUETOOTH_SETTINGS, asAnotherTask)
+            }
+            "data_roaming" -> {
+                openSettings(Settings.ACTION_DATA_ROAMING_SETTINGS, asAnotherTask)
+            }
+            "date" -> {
+                openSettings(Settings.ACTION_DATE_SETTINGS, asAnotherTask)
+            }
+            "display" -> {
+                openSettings(Settings.ACTION_DISPLAY_SETTINGS, asAnotherTask)
+            }
+            "notification" -> {
+                openNotification(asAnotherTask)
+            }
+            "nfc" -> {
+                openSettings(Settings.ACTION_NFC_SETTINGS, asAnotherTask)
+            }
+            "sound" -> {
+                openSettings(Settings.ACTION_SOUND_SETTINGS, asAnotherTask)
+            }
+            "internal_storage" -> {
+                openSettings(Settings.ACTION_INTERNAL_STORAGE_SETTINGS, asAnotherTask)
+            }
+            "battery_optimization" -> {
+                openSettings(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS, asAnotherTask)
+            }
+            "vpn" ->
+              if (Build.VERSION.SDK_INT >= 24) {
+                  openSettings(Settings.ACTION_VPN_SETTINGS, asAnotherTask)
+              } else {
+                  openSettings("android.net.vpn.SETTINGS", asAnotherTask)
+              }
+            "app_settings" -> {
+                openAppSettings(asAnotherTask)
+            }
+            "device_settings" -> {
+                openSettings(Settings.ACTION_SETTINGS, asAnotherTask)
+            }
+            "accessibility" -> {
+                openSettings(Settings.ACTION_ACCESSIBILITY_SETTINGS, asAnotherTask)
+            }
+            else -> {
+                throw NotImplementedError()
+            }
         }
-        else if (call.method == "device_settings") {
-            openSettings(Settings.ACTION_SETTINGS, asAnotherTask)
-        }
-        else if (call.method == "accessibility") {
-            openSettings(Settings.ACTION_ACCESSIBILITY_SETTINGS, asAnotherTask)
+    }
+
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        try {
+            handle(call = call)
+            result.success(null)
+        } catch (e: NotImplementedError) {
+            result.notImplemented()
+        } catch (e: Throwable) {
+            result.error("",e.localizedMessage, null)
         }
     }
 }
--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -10,152 +10,203 @@ class AppSettings {
   static Future<void> openWIFISettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('wifi', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'wifi', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open wireless settings.
   static Future<void> openWirelessSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('wireless', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'wireless', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open location settings.
   static Future<void> openLocationSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('location', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'location', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open security settings.
   static Future<void> openSecuritySettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('security', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'security', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open bluetooth settings.
   static Future<void> openBluetoothSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('bluetooth', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'bluetooth', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open data roaming settings.
   static Future<void> openDataRoamingSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('data_roaming', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'data_roaming', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open date settings.
   static Future<void> openDateSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('date', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'date', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open display settings.
   static Future<void> openDisplaySettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('display', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'display', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open notification settings.
   static Future<void> openNotificationSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('notification', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'notification', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open sound settings.
   static Future<void> openSoundSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('sound', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'sound', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open internal storage settings.
   static Future<void> openInternalStorageSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('internal_storage', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'internal_storage', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open battery optimization settings.
   static Future<void> openBatteryOptimizationSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('battery_optimization', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'battery_optimization', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open app specific settings screen.
   static Future<void> openAppSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('app_settings', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'app_settings', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open NCF settings.
   static Future<void> openNFCSettings({
     bool asAnotherTask = false,
   }) async {
-    _channel.invokeMethod('nfc', {
-      'asAnotherTask': asAnotherTask,
-    });
+    await _channel.invokeMethod<void>(
+      'nfc', 
+      {
+        'asAnotherTask': asAnotherTask,
+      }
+    );
   }
 
   /// Future async method call to open Device settings.
   static Future<void> openDeviceSettings({
       bool asAnotherTask = false,
-    }) async {
-      _channel.invokeMethod('device_settings', {
+  }) async {
+    await _channel.invokeMethod<void>(
+      'device_settings', 
+      {
         'asAnotherTask': asAnotherTask,
-      });
+      }
+    );
   }
 
   /// Future async method call to open VPN settings.
   static Future<void> openVPNSettings({
       bool asAnotherTask = false,
-    }) async {
-      _channel.invokeMethod('vpn', {
+  }) async {
+    await _channel.invokeMethod<void>(
+      'vpn', 
+      {
         'asAnotherTask': asAnotherTask,
-      });
+      }
+    );
   }
 
    /// Future async method call to open accessibility settings.
   static Future<void> openAccessibilitySettings({
       bool asAnotherTask = false,
-    }) async {
-      _channel.invokeMethod('accessibility', {
+  }) async {
+    await _channel.invokeMethod<void>(
+      'accessibility', 
+      {
         'asAnotherTask': asAnotherTask,
-      });
+      }
+    );
   }
 }
